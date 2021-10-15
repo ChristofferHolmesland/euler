87a59d32dd041c073c9a7510e75d3b60727b19f1
Christoffer Holmesland <ChristofferHolmesland@users.noreply.github.com>
2021-10-15 21:11:05 +0200

    Load the first 50 problems on startup

--- preload.js
+++ preload.js
@@ -1,10 +1,68 @@
-window.addEventListener("DOMContentLoaded", () => {
-	const replaceText = (selector, text) => {
-		const element = document.getElementById(selector);
-		if (element) element.innerText = text;
+const axios = require("axios");
+const htmlparser = require("htmlparser");
+const select = require("soupselect").select;
+
+async function getSiteDOM(url) {
+	const resp = await axios({
+		method: "GET",
+		url: url
+	});
+
+	const htmlHandler = new htmlparser.DefaultHandler(function(error, dom) {
+		return dom;
+	});
+
+	const htmlParser = new htmlparser.Parser(htmlHandler);
+	htmlParser.parseComplete(resp.data);
+
+	return htmlHandler.dom;
+}
+
+function getProblems(page) {
+	const tds = select(page, "td");
+	const problems = new Array(tds.length / 3);
+
+	for (let i = 0; i < tds.length; i += 3) {
+		problems[i / 3] = {
+			id: tds[i].children[0].data,
+			title: tds[i+1].children[0].children[0].data,
+			solvedBy: tds[i+2].children[0].children[0].data
+		};
 	}
-  
-	for (const dependency of ["chrome", "node", "electron"]) {
-		replaceText(`${dependency}-version`, process.versions[dependency]);
+
+	return problems;
+}
+
+function displayProblemsList(problems) {
+	const problemList = document.getElementById("problem-list");
+
+	for (let i = 0; i < problems.length; i++) {
+		const id = document.createElement("span");
+		const title = document.createElement("span");
+		const solvedBy = document.createElement("span");
+
+		id.innerHTML = problems[i].id;
+		title.innerHTML = problems[i].title;
+		solvedBy.innerHTML = problems[i].solvedBy;
+
+		const listItem = document.createElement("div");
+		listItem.classList.add("problem-list-item");
+
+		listItem.appendChild(id);
+		listItem.appendChild(title);
+		listItem.appendChild(solvedBy);
+
+		problemList.appendChild(listItem);
 	}
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+	const frontPage = await getSiteDOM("https://projecteuler.net/archives");
+
+	const paginationDiv = select(frontPage, "div.pagination");
+	const numPages = paginationDiv[0].children.length - 1;
+
+	const problems = [...getProblems(frontPage)];
+
+	displayProblemsList(problems);
 });
